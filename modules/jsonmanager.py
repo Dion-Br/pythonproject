781ea9d206fa74a64a72bde7429ea146d052b81e
Dion Brovina <s129267@ap.be>
2023-05-24 14:29:23 +0200

    commands updated

--- modules/jsonmanager.py
+++ modules/jsonmanager.py
@@ -1,10 +1,15 @@
 import json
+import os
 
 
 def executeJSON(jsonstr):
     # JSON commandos uitlezen
     dict_commands = readJSON(jsonstr)
-    print(dict_commands["commandos"])
+
+    for item in dict_commands["commands"]:
+        print(f"Name: {item['name']} => Command: {item['command']} \nResult:")
+        result = os.popen(item["command"]).read()
+        print(result)
 
 
 def readJSON(jsonstr):
